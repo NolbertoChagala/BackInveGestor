44b9443a6988be54ab1f5162a16f69559874792c
Anthony <anthowill1608@gmail.com>
2025-03-15 16:30:17 -0500

    Se agregó DTOs para crear y editar producto

---
+++ DTOs/ProductDTO/ProductEditDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace backend_gestorinv.DTOs.ProductDTO
+{
+    public class ProductEditDTO
+    {
+        public string? producto { get; set; }
+        public int? stock { get; set; }
+        public decimal? precio_unitario { get; set; }
+        public int? proveedor_id { get; set; }
+        public int? categoria_id { get; set; }
+    }
+}
