7ddf434911f57eb15abd8c86faf5fc369d186148
Anthony <anthowill1608@gmail.com>
2025-03-16 17:45:22 -0500

    Se agregaron nuevos DTO en el módulo de inventario

---
+++ DTOs/ProductDTO/ProductDetailsDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace backend_gestorinv.DTOs.ProductDTO
+{
+    public class ProductDetailsDTO
+    {
+        public int id_producto { get; set; }
+        public string producto { get; set; }
+        public int stock { get; set; }
+        public decimal precio_unitario { get; set; }
+        public int proveedor_id { get; set; }
+        public string proveedor { get; set; }
+        public int? categoria_id { get; set; }
+        public string categoria { get; set; }
+    }
+}
---
+++ DTOs/ProductDTO/ProductGetDTO.cs
@@ -0,0 +1,15 @@
+﻿using backend_gestorinv.Models.Domain;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend_gestorinv.DTOs.ProductDTO
+{
+    public class ProductGetDTO
+    {
+        public int id_producto { get; set; }
+        public string producto { get; set; }
+        public int stock { get; set; }
+        public decimal precio_unitario { get; set; }
+        public string proveedor { get; set; }
+        public string categoria { get; set; }
+    }
+}
