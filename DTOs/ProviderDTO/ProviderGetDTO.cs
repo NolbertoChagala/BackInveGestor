6b627ee04d17a3e4fc5a080835fc265d2be2320b
Anthony <anthowill1608@gmail.com>
2025-03-16 17:46:03 -0500

    Se agregaron nuevos DTO en el módulo de proveedores

---
+++ DTOs/ProviderDTO/ProviderGetDTO.cs
@@ -0,0 +1,14 @@
+﻿using backend_gestorinv.Models.Domain;
+
+namespace backend_gestorinv.DTOs.ProviderDTO
+{
+    public class ProviderGetDTO
+    {
+        public int id_proveedor { get; set; }
+        public string proveedor { get; set; }
+        public string telefono { get; set; }
+        public string correo { get; set; }
+        public string direccion { get; set; }
+        public ICollection<ProductsDTO> productos { get; set; }
+    }
+}
