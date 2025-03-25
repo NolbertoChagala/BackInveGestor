7c2b9e1194cd3cc5ecf59eadcb43313125716fa2
Anthony <anthowill1608@gmail.com>
2025-03-25 13:48:14 -0500

    Se creó el nuevo modelo para los Logs

---
+++ Models/Domain/Log.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend_gestorinv.Models.Domain
+{
+    public class Log
+    {
+        [Key]
+        public int id_log {  get; set; }
+        public string mensaje { get; set; } = string.Empty;
+        public string? stack_trace { get; set; }
+        public string endpoint { get; set; } = string.Empty;
+        public int status_code { get; set; }
+        public DateTime fecha_registro { get; set; } = DateTime.Now;
+    }
+}
